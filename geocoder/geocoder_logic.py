ae6eda549824ae03095a82f0f3b0e363d476b81d
ivan <ibodnar@outlook.com>
2017-01-09 20:04:37 -0300

    Agregado class Calles en geocoder_logic

--- geocoder/geocoder_logic.py
+++ geocoder/geocoder_logic.py
@@ -1,3 +1,4 @@
+from django.db import connection
 from .models import CallesGeocod
 
 
@@ -8,3 +9,25 @@ def get_calles():
     """
     query = CallesGeocod.objects.order_by('nombre').distinct('nombre')
     return [calle.nombre for calle in query if calle.nombre is not None]
+
+
+class Calle:
+
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+    def ubicar_altura(self, altura):
+        query = "select altura_direccion_calle(%s, %s)"
+        with connection.cursor() as cursor:
+            cursor.execute(query, [self.nombre, altura])
+            resultado = cursor.fetchone()[0]
+
+        return resultado
+
+    def __add__(self, other):
+        query = "select punto_interseccion(%s, %s)"
+        with connection.cursor() as cursor:
+            cursor.execute(query, [self.nombre, other.nombre])
+            resultado = cursor.fetchone()[0]
+
+        return resultado
