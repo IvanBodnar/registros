a53a0011eaec2aafe9e6860824728277218bb2c8
ivan <ibodnar@outlook.com>
2017-01-10 18:17:28 -0300

    Agregados docstrings en class Calle

--- geocoder/geocoder_logic.py
+++ geocoder/geocoder_logic.py
@@ -15,24 +15,51 @@ def get_calles():
 class Calle:
 
     def __init__(self, nombre):
+        """
+        Takes a street name and checks its existence
+        :param nombre: str
+        """
         try:
             query = "select existe_calle(%s)"
             self._ejecutar_query(query, nombre)
-            self.nombre = nombre
+            self.nombre = nombre.lower()
         except:
             raise CalleNoExiste('La calle no existe')
 
     def _ejecutar_query(self, query, *args):
+        """
+        Executes the passed query
+        :param query: query string properly formatted
+        with %s as parameter placeholders:
+        'select function(%s [...%s])'
+        :param args: arguments to be passed to the query
+        :return : query result. None if no result was retrieved.
+        """
         with connection.cursor() as cursor:
             cursor.execute(query, args)
             resultado = cursor.fetchone()[0]
         return resultado
 
     def ubicar_altura(self, altura):
+        """
+        Returns a string representation of the geometry
+        in wgs84 crs of the point that marks
+        the house number passed.
+        :param altura int: house number
+        :return str: string representation of geometry
+        """
         query = "select altura_direccion_calle(%s, %s)"
         return self._ejecutar_query(query, self.nombre, altura)
 
     def __add__(self, other):
+        """
+        Overloads + operator to allow the return of the geometry
+        of an intersection by adding two Calle instances:
+        street1 + street2
+        :param other: the other Calle instance
+        :return str: string representation of the point marking
+        the intersection in wgs84 crs.
+        """
         query = "select punto_interseccion(%s, %s)"
         return self._ejecutar_query(query, self.nombre, other.nombre)
 
