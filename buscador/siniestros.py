ed5170a03bb86a224857307beab784811660daa2
ivan <ibodnar@outlook.com>
2017-01-13 10:02:44 -0300

    Trabajando en Siniestros._filtrar_hechos()

--- buscador/siniestros.py
+++ buscador/siniestros.py
@@ -1,19 +1,28 @@
 from .models import Hechos
 from django.contrib.gis.geos import GEOSGeometry, Point
+from django.contrib.gis.db.models.functions import Transform
 
 
 class Siniestros:
 
     def __init__(self, punto, radio, anios):
-        self.punto = punto
+        self.punto = self.punto_a_3857(punto)
         self.radio = radio
         self.anios = anios
 
-    def _filtrar_anios(self):
-        return Hechos.objects.filter(anio__in=self.anios)
+    def _filtrar_hechos(self):
+        qs = Hechos.objects\
+            .filter(anio__in=self.anios)\
+            .exclude(geom__isnull=True)\
+            .annotate(transform=Transform('geom', srid=3857))\
+            .all()
+        for q in qs:
+            q.geom = q.transform
+        return qs
 
-    def punto_a_3857(self, punto):
+    @staticmethod
+    def punto_a_3857(punto):
         ge = GEOSGeometry(punto, srid=4326)
         ge.transform(3857)
-        return ge.x, ge.y
+        return ge
 
