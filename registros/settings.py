e830d7eafd10349fd45e5f80546c8579ac7a0b2c
ivan <ibodnar@outlook.com>
2017-01-28 18:14:58 -0300

    Cambio en STATIC_ROOT

--- registros/settings.py
+++ registros/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.environ.get('STATIC_ROOT', None)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Para que funcionen los test
 class DisableMigrations(object):
