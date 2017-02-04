8944d7e1ad302318ffe7f2cded278329bcc4a189
ivan <ibodnar@outlook.com>
2017-02-04 19:50:20 -0300

    Agregado borde en tabla y mapa - nav-pills en vez de nav-tabs

--- buscador/static/buscador/map.js
+++ buscador/static/buscador/map.js
@@ -1,6 +1,8 @@
 var map = L.map('map', {
-    maxZoom: 18
-}).setView([-34.615715, -58.451204], 12);
+    zoom: 12,
+    center: [-34.615715, -58.451204],
+    maxZoom: 21
+})//.setView([-34.615715, -58.451204], 12);
 
 //
 // Levanta y parsea la var geojson de tabla_buscador.html,
@@ -20,7 +22,7 @@ $("#mapa_tab").on('shown.bs.tab', function() {
 $(function() {
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
+    maxZoom: 21,
     id: 'ivangbodnar.p7n41boc',
     accessToken: 'pk.eyJ1IjoiaXZhbmdib2RuYXIiLCJhIjoiY2lnaXR0YzVvMDAwNXVha3JsZnFlZzBjbyJ9.X17WT4iMx_powofqtqKkDg'
 }).addTo(map);
