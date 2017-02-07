675925a243fc0a1efadfa05a8afa1db510ab3e9e
ivan <ibodnar@outlook.com>
2017-02-07 11:07:33 -0300

    Agregado UserStats model en user.models

--- user/models.py
+++ user/models.py
@@ -1,5 +1,23 @@
+import maya
+from django.contrib.auth.models import User
 from django.contrib.gis.db import models
+from django.contrib.postgres.fields import ArrayField
 
 
 class UserStats(models.Model):
-    pass
+
+    @staticmethod
+    def hora_argentina():
+        return maya.now().datetime(to_timezone='America/Argentina/Buenos_Aires', naive=True)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    session_key = models.CharField(max_length=40, null=True)
+    fh = models.DateTimeField(default=hora_argentina)
+    calle1 = models.CharField(max_length=100, null=True)
+    calle2 = models.CharField(max_length=100, null=True)
+    radio = models.IntegerField(null=True)
+    anios = ArrayField(models.IntegerField(null=True))
+    geom = models.PointField(null=True, srid=4326)
+
+
+
