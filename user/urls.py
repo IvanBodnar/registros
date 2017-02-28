dec0f97026c41828833a665b8d229e1108d2f338
ivan <ibodnar@outlook.com>
2017-02-28 17:36:48 -0300

    Previo a cambio de base.html

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.forms import AuthenticationForm
+from .views import inicio
 
 AuthenticationForm.error_messages = error_messages = {
         'invalid_login': (
@@ -13,7 +14,8 @@ AuthenticationForm.error_messages = error_messages = {
 urlpatterns = [
     url(r'^login/$', auth_views.login,
         {'template_name': 'user/login.html',
-         'extra_context': {'next': 'buscador_ingresar_calle'}},
+         'extra_context': {'next': 'user_inicio'}},
         name='login'),
     url(r'^logout/$', auth_views.logout_then_login, name='logout'),
+    url(r'^inicio/$', inicio, name='user_inicio')
 ]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def inicio(request):
+    return render(request, template_name='user/inicio.html')
