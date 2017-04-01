14dfeb0b1ab564abddf1d4a9afb0ca913dec3006
ivan <ibodnar@outlook.com>
2017-04-01 16:42:15 -0300

    Cambios en environ 1

--- registros/environment/environ_dev.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-export SECRET_KEY="m78-jvb2n9&-y)65u0r+mft%ogwuwy&(e*1d7k0kzwv^khxiwg"
-export DEBUG="True"
-export NAME="geocoder"
-export USER="ivan"
-export PASSWORD="ivan"
-export PORT="5432"
