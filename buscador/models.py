da174a4bedb657efc8274a911a780b7bc1c4cf93
ivan <ibodnar@outlook.com>
2017-04-02 12:49:31 -0300

    Agregado 2016 a la db

--- buscador/models.py
+++ buscador/models.py
@@ -42,6 +42,7 @@ class Hechos(models.Model):
     participantes = ArrayField(models.CharField(max_length=100, blank=True, null=True, default=None))
     geom_3857 = models.PointField(blank=True, null=True, srid=3857)
     causa = models.CharField(max_length=20, blank=True, null=True)
+    tipo_colision1 = models.CharField(max_length=60, blank=True, null=True)
 
     def __str__(self):
         return '{}'.format(self.direccion_normalizada)
